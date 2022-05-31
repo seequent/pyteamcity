e18dbc26df40d0dfebb7f50e7493614835845cab
Logan Beard <logan.beard@seequent.com>
2022-06-01 10:41:43 +1200

    LF-44544 remove print statement

--- pyteamcity/future/build_type.py
+++ pyteamcity/future/build_type.py
@@ -99,7 +99,6 @@ class BuildType(object):
             headers={'Content-Type': 'application/xml'}
         )
         raise_on_status(res)
-        print(res.text)
         json = res.json()
         triggers = json.get('trigger', [])
         return triggers
